441bda9b5d1ebc745b6fb1b8c0601a69a2f9531e
BaeGayeon <ebgy918@gmail.com>
2023-05-08 21:32:19 +0900

    refactor: 장학금 필터링, 정 리팩토링

--- controllers/scholarshipController.js
+++ controllers/scholarshipController.js
@@ -2,6 +2,8 @@ const baseResponse = require('../config/baseResponseStatus');
 const { response, errResponse } = require('../config/response');
 const scholarshipService = require('../services/scholarshipService');
 const url = require('url');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
 module.exports = {
     getScholarships: async (req, res) => {
@@ -10,10 +12,12 @@ module.exports = {
         let type = urlObj.query.type;
         let ordering = urlObj.query.ordering;
         if (type == undefined) {
-            type = 'all';
+            type = { [Op.not]: null };
         }
         if (ordering == undefined) {
-            ordering = 'new';
+            ordering = ['id'];
+        } else {
+            ordering = [ordering];
         }
         let findResponse = await scholarshipService.findScholarship(
             type,
--- services/scholarshipService.js
+++ services/scholarshipService.js
@@ -3,6 +3,7 @@ const { response, errResponse } = require('../config/response');
 const sequelize = require('sequelize');
 const models = require('../models');
 const Scholarship = models.scholarships;
+const Op = sequelize.Op;
 
 module.exports = {
     findScholarship: async (type, ordering) => {
@@ -12,52 +13,13 @@ module.exports = {
                 sequelize.col('end_date'),
                 sequelize.fn('NOW')
             );
-            let findResult;
-            if (type == 'all' && ordering == 'new') {
-                findResult = await Scholarship.findAll({
-                    attributes: [
-                        'title',
-                        'institution',
-                        'type',
-                        [dday, 'dday'],
-                    ],
-                });
-            } else if (type != 'all' && ordering == 'new') {
-                findResult = await Scholarship.findAll({
-                    attributes: [
-                        'title',
-                        'institution',
-                        'type',
-                        [dday, 'dday'],
-                    ],
-                    where: {
-                        type: type,
-                    },
-                });
-            } else if (type == 'all' && ordering != 'new') {
-                findResult = await Scholarship.findAll({
-                    attributes: [
-                        'title',
-                        'institution',
-                        'type',
-                        [dday, 'dday'],
-                    ],
-                    order: [ordering],
-                });
-            } else {
-                findResult = await Scholarship.findAll({
-                    attributes: [
-                        'title',
-                        'institution',
-                        'type',
-                        [dday, 'dday'],
-                    ],
-                    where: {
-                        type: type,
-                    },
-                    order: [ordering],
-                });
-            }
+            let findResult = await Scholarship.findAll({
+                attributes: ['title', 'institution', 'type', [dday, 'dday']],
+                where: {
+                    type: type,
+                },
+                order: ordering,
+            });
             return response(baseResponse.SUCCESS, findResult);
         } catch (err) {
             console.log(err);
